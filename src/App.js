7470dbf14a7ce8cb3b74eeff7ce2e292c245690c
Nick <ntoukas.nick@gmail.com>
2023-01-27 21:59:44 -0500

    fetch current weather data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Cities from './components/Cities';
+import WeatherDashboard from './components/WeatherDashboard';
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=ottawa&appid=6507ec9c905f52f3550a3737b29ef566`;
+
+const fetchData = async () => {
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 function App() {
-  return <div>weather app</div>;
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <main className='wrapper'>
+      <article className='app-container'>
+        <Cities />
+        <WeatherDashboard />
+      </article>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Cities.js
@@ -0,0 +1,18 @@
+const Cities = () => {
+  return (
+    <nav className='nav'>
+      <ul className='cities-list'>
+        <li className='city'>
+          <a href='#'>Ottawa</a>
+        </li>
+        <li className='city'>
+          <a href='#'>Moscow</a>
+        </li>
+        <li className='city'>
+          <a href='#'>Tokyo</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Cities;
